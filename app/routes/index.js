0a0242342ed7f6599e16aac42d5093e213af6257
Pallavi TS <pallavi046@gmail.com>
2015-08-24 18:29:07 +0530

    Fetched users from github api

---
+++ app/routes/index.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model: function(){
+        return Ember.$.getJSON('https://api.github.com/users').then(function(data) {
+            return data;
+        });
+
+    }
+});
